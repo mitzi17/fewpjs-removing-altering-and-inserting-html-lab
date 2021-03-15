4abe6da86d717aa6598ebcf6980e406b17f77352
Mitzi17 <m_estrada17@live.com>
2021-03-14 22:38:34 -0700

    commit tree 503a6c1778738bfd52ca2f0e0f1794632a5f6d1a

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+let element = document.querySelector("main#main")
+element.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!"
